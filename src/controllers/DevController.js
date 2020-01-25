5508a1969a340a084bb2028422932e29557e7289
Gabriel Nicacio firmino <gabrielnicaciofirmino@MacBook-de-Gabriel.local>
2020-01-25 01:27:27 -0300

    fix model avatar_url

--- src/controllers/DevController.js
+++ src/controllers/DevController.js
@@ -16,7 +16,7 @@ module.exports = {
     if (!dev) {
       const response = await axios.get('https://api.github.com/users/' + github_username);
      
-      const { name = login, avatar_url, bio } = response.data
+      const { name = github_username, avatar_url, bio } = response.data;
     
       const techsArray = parseStringAsArray(techs);
     
