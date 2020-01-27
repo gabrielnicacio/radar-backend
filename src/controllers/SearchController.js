e1a4634636b929dcba6af8a205b4ec9b42054823
Gabriel Nicacio firmino <gabrielnicaciofirmino@MacBook-de-Gabriel.local>
2020-01-27 17:42:17 -0300

    fix search controller

--- src/controllers/SearchController.js
+++ src/controllers/SearchController.js
@@ -4,23 +4,23 @@ module.exports = {
   //busca todos devs num raio de 10km
   //filtrar por tecnologias
   async index(req, res) {
-    const { latitude, longitude, techs } = req.query;
+    const { latitude, longitude, techs } = req.body;
 
     const techsArray = parseStringAsArray(techs);
 
     const devs = await Dev.find({
       techs: {
         $in: techsArray,
-        location: {
-          $near: {
-            $geometry: {
-              type: 'Point',
-              coordinates: [longitude, latitude],
-            },
-            $maxDistance: 10000,
-          }
-        }
       },
+      location: {
+        $near: {
+          $geometry: {
+            type: 'Point',
+            coordinates: [longitude, latitude],
+          },
+          $maxDistance: 10000,
+        }
+      }
     })
 
     return res.json({ devs: devs });
